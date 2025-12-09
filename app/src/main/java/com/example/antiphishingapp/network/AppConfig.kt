aaa91909889c3890160d60a0836261d032b57858
HyeonSoo Choi <alanghs1111@gmail.com>
2025-12-10 00:39:32 +0900

    fix: 소셜 로그인 리다이렉트 오류 수정 및 앱 스키마 적용 (#57)
    [변경 사항]
    - AppConfig: Redirect URI를 웹(localhost)에서 앱 스키마(antiphishing://)로 변경
    - AndroidManifest: MainActivity에 singleTask 모드 및 intent-filter 통합 적용
    - MainActivity: 딥링크 수신 로직(handleSocialLoginIntent) 추가
    
    [해결 문제]
    - 소셜 로그인 연결 오류 해결
    
    Closes #57

--- app/src/main/java/com/example/antiphishingapp/network/AppConfig.kt
+++ app/src/main/java/com/example/antiphishingapp/network/AppConfig.kt
@@ -7,12 +7,12 @@ object AppConfig {
 
     // --- 카카오 설정 ---
     const val KAKAO_CLIENT_ID = "cc9b60d42f687ae216470e85c98822f0"
-    const val KAKAO_REDIRECT_URI = "http://localhost:8000/auth/kakao/callback"
+    const val KAKAO_REDIRECT_URI = "antiphishing://oauth/kakao/callback"
     const val KAKAO_AUTH_URL = "https://kauth.kakao.com/oauth/authorize"
 
     // --- 네이버 설정 ---
     const val NAVER_CLIENT_ID = "17faQV8AGa02hywfsUTn"
     const val NAVER_CLIENT_SECRET = "vnimMcgfoL"
-    const val NAVER_REDIRECT_URI = "http://localhost:8000/auth/naver/callback"
+    const val NAVER_REDIRECT_URI = "antiphishing://oauth/naver/callback"
     const val NAVER_AUTH_URL = "https://nid.naver.com/oauth2.0/authorize"
 }
